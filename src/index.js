80ebad1461c014d2dbbebfadd8d641bdce8ecfde
dilipreno <dilipraoa@gmail.com>
2020-05-25 20:33:47 +0530

    reactrobo

--- src/index.js
+++ src/index.js
@@ -5,5 +5,22 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleWare from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleWare, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
